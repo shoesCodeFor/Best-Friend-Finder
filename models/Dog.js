3988d5698a329a0085b385953063e6dced9b5f4f
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-10-17 23:26:28 -0600

    Add class template for Dog
    Pre typscript

---
+++ models/Dog.js
@@ -0,0 +1,20 @@
+class Dog{
+    constructor(name, scoringObj, image){
+
+    }
+    name;
+    habitatSize;
+    energy;
+    independence;
+    trainability;
+    kids;
+    otherDogs;
+    temperment;
+    loyalty;
+    size;
+    longevity;
+    climate;
+    image; 
+}
+
+export default Dog;
